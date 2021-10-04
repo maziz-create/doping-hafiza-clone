c47e46e069b6b0226c97555ca6eb8b3a406d6671
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-04 20:09:26 +0300

    Integrated Homepage Component in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+//css
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+//pages
+import Homepage from './pages/Homepage'
 
 function App() {
   return (
     <div>
-      
+      <Homepage />
     </div>
   );
 }
