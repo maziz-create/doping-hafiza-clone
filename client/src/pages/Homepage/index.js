b4f1e99c05590d00903bd2a03df80a7fe332f016
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-04 20:09:02 +0300

    Added Homepage Component and integrated HomepageNavbar into it.

---
+++ client/src/pages/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import HomepageNavbar from '../../components/HomepageNavbar'
+
+function Homepage() {
+    return (
+        <div>
+            <HomepageNavbar />
+        </div>
+    )
+}
+
+export default Homepage
