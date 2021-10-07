e876d47ef41267ff688a3e6c056866163ab02b33
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-07 12:15:08 +0300

    Added carousel item contents and its styling css codes

--- client/src/pages/Homepage/HomepageCarousel/index.js
+++ client/src/pages/Homepage/HomepageCarousel/index.js
@@ -39,21 +39,22 @@ function HomepageCarousel() {
             interval={null}
             keyboard={false}
         >
+            {/* Carousel 1 */}
             <Carousel.Item
                 style={{ height: '80vh' }}
             >
-                <div className="position-absolute homepage-carousel-1-div" style={{
+                <div className="position-absolute homepage-carousel-div-26rem" style={{
                     color: '#fff',
                 }}>
-                    <h3 className="homepage-carousel-1-div-heading">
+                    <h3 className="homepage-carousel-div-heading-letter4">
                         TYT - AYT Kaç Günde Biter?
                     </h3>
-                    <p className="homepage-carousel-1-div-text">
+                    <p className="homepage-carousel-div-text">
                         Doping Hafıza eğitimleri ile{' '}
                         <strong>
                             TYT-AYT'yi kaç günde
                         </strong>
-                        bitirebileceğinizi hesaplayabilirsiniz!
+                        {' '}bitirebileceğinizi hesaplayabilirsiniz!
                     </p>
                     <Button variant="light px-4 py-3 homepage-carousel-1-div-button">
                         Hemen Hesaplayın!
@@ -61,37 +62,83 @@ function HomepageCarousel() {
                 </div>
                 <img
                     className="d-block ms-auto me-5"
-                    style={{ height: '20rem' }}
+                    style={{ height: '27rem' }}
                     src={home_slider_one}
                     alt="First slide"
                 />
             </Carousel.Item>
+            {/* Carousel 2 */}
             <Carousel.Item
                 style={{ height: '80vh' }}
             >
+                <div className="position-absolute homepage-carousel-div-26rem" style={{
+                    color: '#fff',
+                }}>
+                    <h3 className="homepage-carousel-div-heading-letter0">
+                        Eğitimlerimizde Hangi Methodları Kullanıyoruz?
+                    </h3>
+                    <p className="homepage-carousel-div-text">
+                        Örnek eğitim videolarımızı inceleyerek <br />
+                        Doping Hafıza Yeni Nesil Online Eğitim Platformu'nda <br />
+                        sizi neler bekliyor görebilirsiniz!
+                    </p>
+                    <Button variant="primary" className="px-4 py-3 homepage-carousel-2-div-button">
+                        Örnek Eğitim Videolarını İzleyin!
+                    </Button>
+                </div>
                 <img
                     className="d-block ms-auto me-5"
-                    style={{ height: '20rem' }}
+                    style={{ height: '30rem' }}
                     src={home_slider_two}
                     alt="Second slide"
                 />
             </Carousel.Item>
+            {/* Carousel 3 */}
             <Carousel.Item
                 style={{ height: '80vh' }}
             >
+                <div className="position-absolute homepage-carousel-div-28rem" style={{
+                    color: '#fff',
+                }}>
+                    <h3 className="homepage-carousel-div-heading-letter0">
+                        2022 YKS'de Erken Kayıt Avantajları!
+                    </h3>
+                    <p className="homepage-carousel-div-text">
+                        Doping Hafıza ile <br />
+                        Sınav Sonuçlarınızda Fark Yaratın!
+                    </p>
+                    <Button variant="primary" className="px-4 py-3 homepage-carousel-2-div-button">
+                        Detayları İnceleyin!
+                    </Button>
+                </div>
                 <img
                     className="d-block ms-auto me-5"
-                    style={{ height: '20rem' }}
+                    style={{ height: '30rem' }}
                     src={home_slider_three}
                     alt="Third slide"
                 />
             </Carousel.Item>
+            {/* Carousel 4 */}
             <Carousel.Item
                 style={{ height: '80vh' }}
             >
+                <div className="position-absolute homepage-carousel-div-28rem" style={{
+                    color: '#fff',
+                }}>
+                    <h3 className="homepage-carousel-div-heading-letter0">
+                        2021 YKS Sonuçları Açıklandı!
+                    </h3>
+                    <p className="homepage-carousel-div-text">
+                        Öğrencilerimiz her sene Türkiye <br />
+                        derecelerine imza atıyor!
+                    </p>
+                    <Button variant="primary" className="px-4 py-3 homepage-carousel-2-div-button">
+                        Derece Yapan Öğrencilerimiz
+                    </Button>
+                </div>
                 <img
                     className="d-block ms-auto me-5"
-                    style={{ height: '20rem' }}
+                    style={{ height: '26rem' }}
                     src={home_slider_four}
                     alt="Third slide"
                 />
