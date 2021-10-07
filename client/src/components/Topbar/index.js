1f05d2216771486bea05657322f2e935966ab862
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-07 12:24:30 +0300

    Added topbar and integrated in Homepage Component

---
+++ client/src/components/Topbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Topbar() {
+    return (
+        <div style={{color: '#fff'}}>
+            Topbar
+        </div>
+    )
+}
+
+export default Topbar
--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Topbar from '../../components/Topbar'
 import HomepageNavbar from '../../pages/Homepage/HomepageNavbar'
 import HomepageCarousel from '../../pages/Homepage/HomepageCarousel'
 import HomepageBackground from '../../pages/Homepage/HomepageBackground'
@@ -8,6 +9,7 @@ function Homepage() {
     return (
         <div>
             <HomepageBackground />
+            <Topbar />
             <HomepageNavbar />
             <HomepageCarousel />
         </div>
