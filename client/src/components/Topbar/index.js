625360d7f55ae589ce7650a6f5efca8cf5867172
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-07 15:18:36 +0300

    Added topbar and its styling css

--- client/src/components/Topbar/index.js
+++ client/src/components/Topbar/index.js
@@ -1,9 +1,60 @@
 import React from 'react'
 
+import './style.css'
+
+
 function Topbar() {
     return (
-        <div style={{color: '#fff'}}>
-            Topbar
+        <div className="topbar mb-3">
+            <div className="contacts">
+                <div className="contact wp">
+                    <div className="icon">
+                        Icon
+                    </div>
+
+                    <p>
+                        0 533 236 20 05
+                    </p>
+                </div>
+                <div className="contact phone">
+                    <div className="icon">
+                        Icon
+                    </div>
+
+                    <p>
+                        0 212 236 74 41
+                    </p>
+                </div>
+                <div className="contact email">
+                    <div className="icon">
+                        Icon
+                    </div>
+                    <p>
+                        info@dopinghafiza.com
+                    </p>
+                </div>
+            </div>
+
+            <div className="social-medias">
+                <div className="social-media-icon">
+                    Icon
+                </div>
+                <div className="social-media-icon">
+                    Icon
+                </div>
+                <div className="social-media-icon">
+                    Icon
+                </div>
+                <div className="social-media-icon">
+                    Icon
+                </div>
+                <div className="social-media-icon">
+                    Icon
+                </div>
+                <div className="social-media-icon">
+                    Icon
+                </div>
+            </div>
         </div>
     )
 }
