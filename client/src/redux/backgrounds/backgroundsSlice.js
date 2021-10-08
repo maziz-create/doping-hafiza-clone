f66aff19fe7ee7da2f71d60a648fd0164d6c961f
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-08 15:39:24 +0300

    Added activeBackgroundHeight state. Added changeBackgroundHeightForMobile reducer

--- client/src/redux/backgrounds/backgroundsSlice.js
+++ client/src/redux/backgrounds/backgroundsSlice.js
@@ -3,16 +3,27 @@ import { createSlice } from '@reduxjs/toolkit';
 export const backgroundsSlice = createSlice({
     name: 'backgrounds',
     initialState: {
-        activeBackground: 'tytayt'
+        activeBackground: 'tytayt',
+        activeBackgroundHeight: '47rem',
     },
     reducers: {
         changeActiveBackground: (state, action) => {
             //all bg types = tytayt, egitim, yks2022, yks2021
             const bgType = action.payload;
             state.activeBackground = bgType;
+        },
+        changeBackgroundHeightForMobile: (state, action) => {
+            // console.log('şu anki bg height => ', state.activeBackgroundHeight);
+            const bgHeight = action.payload;
+            if (state.activeBackgroundHeight === '47rem') {
+                state.activeBackgroundHeight = bgHeight;
+            } else {
+                state.activeBackgroundHeight = '47rem';
+            }
+            // console.log('bg height değişti => ', state.activeBackgroundHeight);
         }
     }
 })
 
-export const { changeActiveBackground } = backgroundsSlice.actions;
+export const { changeActiveBackground, changeBackgroundHeightForMobile } = backgroundsSlice.actions;
 export default backgroundsSlice.reducer;
