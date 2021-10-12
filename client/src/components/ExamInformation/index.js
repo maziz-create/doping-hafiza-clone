09fc827faeb4904abe19059b79c1531980698960
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-12 18:22:36 +0300

    Added mockup image in ExamInformation Component

--- client/src/components/ExamInformation/index.js
+++ client/src/components/ExamInformation/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 
 import Row from 'react-bootstrap/Row'
 
+//components
 import ProductButtons from '../ProductButtons/'
+
+//images
+import mockup from '../../assets/images/mockup.webp'
+
+//css
+import './style.css'
 
 function ExamInformation() {
     return (
         <div style={{ paddingTop: '5.5rem' }}>
-            <Row className="col-6">
+            <Row className="col-lg-6">
                 <div
                     style={{
                         fontSize: '10rem',
@@ -32,18 +39,23 @@ function ExamInformation() {
                         En Etkili Yolu!
                     </h2>
                 </div>
-                <div style={{color: '#3A4759'}}>
-                <p>
-                    YKS, LGS, KPSS, DGS ve ALES'e Doping Hafıza ile sistemli, kapsamlı ve verimli bir şekilde hazırlanmak ister misiniz?
-                </p>
-                <p>
-                    Yeni Nesil Online Eğitim Platformumuzda farklı konseptlerle hazırlanmış konu anlatımlarını izledikten sonra video çözümlü testleri çözerek netlerinizi artırabilirsiniz.
+                <div style={{ color: '#3A4759' }}>
+                    <p>
+                        YKS, LGS, KPSS, DGS ve ALES'e Doping Hafıza ile sistemli, kapsamlı ve verimli bir şekilde hazırlanmak ister misiniz?
+                    </p>
+                    <p>
+                        Yeni Nesil Online Eğitim Platformumuzda farklı konseptlerle hazırlanmış konu anlatımlarını izledikten sonra video çözümlü testleri çözerek netlerinizi artırabilirsiniz.
 
-                </p>
+                    </p>
                 </div>
-                
 
                 <ProductButtons />
+                <div className="mockup d-none d-lg-block">
+                    <img
+                        src={mockup}
+                        alt="mockup"
+                    />
+                </div>
             </Row>
         </div>
     )
