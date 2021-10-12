045f55eea3302dba14fb32f5e93c073c9fd92534
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-12 17:11:55 +0300

    Integrated ExamInformation Component in App.js

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -5,6 +5,11 @@ import HomepageNavbar from '../../components/HomepageNavbar'
 import HomepageCarousel from '../../components/HomepageCarousel'
 import HomepageBackground from '../../components/HomepageBackground'
 
+// Homepage Content
+import ExamInformation from '../../components/ExamInformation'
+
+import Container from 'react-bootstrap/Container'
+
 function Homepage() {
     return (
         <div>
@@ -12,6 +17,11 @@ function Homepage() {
             <Topbar />
             <HomepageNavbar />
             <HomepageCarousel />
+            {/* -- */}
+
+            <Container>
+                <ExamInformation />
+            </Container>
         </div>
     )
 }
