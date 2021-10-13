e48deb3ee997014d7eb0bc309228ac8860ca73f1
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-13 10:19:01 +0300

    Added a div for col-lg-6 class.

--- client/src/components/ExamInformation/index.js
+++ client/src/components/ExamInformation/index.js
@@ -13,48 +13,50 @@ import './style.css'
 
 function ExamInformation() {
     return (
-        <div className="d-none d-md-inline-block" style={{ paddingTop: '4.5rem' }}>
-            <Row className="col-lg-6">
-                <div
-                    style={{
-                        fontSize: '10rem',
-                    }}
-                >
-                    <h3
-                        className="fw-bold"
+        <div className="d-none d-md-inline-block" style={{ paddingTop: '4.5rem', marginBottom: '1rem' }}>
+            <Row>
+                <div className="col-lg-6">
+                    <div
                         style={{
-                            color: '#3A4759',
+                            fontSize: '10rem',
                         }}
                     >
-                        Sınavlara Hazırlanmanın
-                    </h3>
-                    <h2
-                        className="fw-bold"
-                        style={{
-                            color: '#CC171B',
-                            marginBottom: '2rem',
-                            fontSize: '2.5rem'
-                        }}
-                    >
-                        En Etkili Yolu!
-                    </h2>
-                </div>
-                <div style={{ color: '#3A4759' }}>
-                    <p>
-                        YKS, LGS, KPSS, DGS ve ALES'e Doping Hafıza ile sistemli, kapsamlı ve verimli bir şekilde hazırlanmak ister misiniz?
-                    </p>
-                    <p>
-                        Yeni Nesil Online Eğitim Platformumuzda farklı konseptlerle hazırlanmış konu anlatımlarını izledikten sonra video çözümlü testleri çözerek netlerinizi artırabilirsiniz.
+                        <h3
+                            className="fw-bold"
+                            style={{
+                                color: '#3A4759',
+                            }}
+                        >
+                            Sınavlara Hazırlanmanın
+                        </h3>
+                        <h2
+                            className="fw-bold"
+                            style={{
+                                color: '#CC171B',
+                                marginBottom: '2rem',
+                                fontSize: '2.5rem'
+                            }}
+                        >
+                            En Etkili Yolu!
+                        </h2>
+                    </div>
+                    <div style={{ color: '#3A4759' }}>
+                        <p>
+                            YKS, LGS, KPSS, DGS ve ALES'e Doping Hafıza ile sistemli, kapsamlı ve verimli bir şekilde hazırlanmak ister misiniz?
+                        </p>
+                        <p>
+                            Yeni Nesil Online Eğitim Platformumuzda farklı konseptlerle hazırlanmış konu anlatımlarını izledikten sonra video çözümlü testleri çözerek netlerinizi artırabilirsiniz.
 
-                    </p>
-                </div>
+                        </p>
+                    </div>
 
-                <ProductButtons />
-                <div className="mockup d-none d-lg-block">
-                    <img
-                        src={mockup}
-                        alt="mockup"
-                    />
+                    <ProductButtons />
+                    <div className="mockup d-none d-lg-block">
+                        <img
+                            src={mockup}
+                            alt="mockup"
+                        />
+                    </div>
                 </div>
             </Row>
         </div>
