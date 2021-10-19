64da42e4c749dd74f01c4895d2aabcae48d2189f
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-19 20:30:59 +0300

    Integrated TrainingCards Component in PreferredTrainings

--- client/src/components/PreferredTrainings/index.js
+++ client/src/components/PreferredTrainings/index.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 
 import TrainingsButtons from './TrainingsButtons'
+import TrainingCards from './TrainingCards'
 
 const PreferredTrainings = () => {
     return (
         <Container>
             <TrainingsButtons />
+            <TrainingCards />
         </Container>
     )
 }
